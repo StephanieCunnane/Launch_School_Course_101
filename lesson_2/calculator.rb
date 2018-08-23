6050b04fdb2a837a39ac98afb1c95c01705525d3
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2018-08-23 00:08:30 +0000

    Refactor calculator program

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -6,6 +6,15 @@ def valid_number?(num)
   num.to_i() != 0
 end
 
+def operation_to_message(op)
+  case op
+  when '1' then "Adding"
+  when '2' then "Subtracting"
+  when '3' then "Multiplying"
+  when '4' then "Dividing"
+  end
+end
+
 prompt("Welcome to Calculator! Enter your name: ")
 
 name = nil
@@ -48,7 +57,16 @@ loop do
     end
   end
 
-  prompt("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
+  # a heredoc; the 3 letters can be anything as long as they match
+  operator_prompt = <<-MSG
+    What operation would you like to perform?
+    1) add
+    2) subtract
+    3) multiply
+    4) divide
+  MSG
+
+  prompt(operator_prompt)
 
   operator = nil
   loop do
@@ -57,6 +75,8 @@ loop do
     break if %w(1 2 3 4).include?(operator)
     prompt("Must choose 1, 2, 3 or 4")
   end
+
+  prompt("#{operation_to_message(operator)} the two numbers...")
 
   result = case operator
            when '1' then number1.to_i() + number2.to_i()

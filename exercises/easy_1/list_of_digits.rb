774ac92f17b70cd0b9dcb00bb4763a9289464eea
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2018-09-09 16:21:22 +0000

    Add list_of_digits.rb

---
+++ exercises/easy_1/list_of_digits.rb
@@ -0,0 +1,17 @@
+# Input: one positive integer
+# Output: an array listing the digits in the given number
+
+# def digit_list(num)
+#   num.to_s.chars.map { |char| char.to_i }
+# end
+
+# More idiomatic solution
+def digit_list(num)
+  num.to_s.chars.map(&:to_i)
+end
+
+puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
+puts digit_list(7) == [7]                     # => true
+puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
+puts digit_list(444) == [4, 4, 4]             # => true
+

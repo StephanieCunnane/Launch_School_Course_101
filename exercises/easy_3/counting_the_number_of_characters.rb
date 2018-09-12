b34594acd1837ec1b9845b3bdb0437dd3fcc8d1a
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2018-09-12 17:19:54 +0000

    Add counting_the_number_of_characters.rb

---
+++ exercises/easy_3/counting_the_number_of_characters.rb
@@ -0,0 +1,9 @@
+# Input:  one string provided by the user
+# Output: one string displaying the number of characters in the input string
+#         (whitespace excluded)
+
+print 'Please write word or multiple words: '
+input_string = gets.chomp
+char_count = input_string.delete(' ').length
+
+puts "There are #{char_count} characters in \"#{input_string}\"."

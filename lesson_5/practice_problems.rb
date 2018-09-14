64398deee9ebc0ff0ffc9ba08f6fbd47ae192998
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2018-09-14 18:00:30 +0000

    Add problems 7 & 8

--- lesson_5/practice_problems.rb
+++ lesson_5/practice_problems.rb
@@ -111,3 +111,44 @@ munsters.each do |name, details|
                     .sub("(age)", details["age"].to_s)
                     .sub("(male or female)", details["gender"])
 end
+
+############################################################
+# 7
+
+# Given this code, what would be the final values of a and b?
+a = 2
+b = [5, 8]
+arr = [a, b]
+
+arr[0] += 2
+arr[1][0] -= a
+
+p a
+p b
+
+# a => 2
+# b => [3, 8]
+
+############################################################
+# 8
+
+# Using the each method, write some code to output all of the vowels from
+# the strings.
+hsh = {first: ['the', 'quick'], second: ['brown', 'fox'], third: ['jumped'],
+       fourth: ['over', 'the', 'lazy', 'dog']}
+
+vowels = 'aeiou'
+
+hsh.values.flatten.join.each_char do |char|
+  puts char if vowels.include?(char)
+end
+
+# or
+
+hsh.each do |_, value|
+  value.each do |str|
+    str.chars.each do |char|
+      puts char if vowels.include?(char)
+    end
+  end
+end

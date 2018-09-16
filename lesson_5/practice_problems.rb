6a852649d77ae3061af7d17359c49dba6143572d
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2018-09-16 21:11:36 +0000

    Add problems 14 & 15

--- lesson_5/practice_problems.rb
+++ lesson_5/practice_problems.rb
@@ -239,3 +239,43 @@ result = arr.sort_by do |sub_arr|
 end
 
 p result
+
+############################################################
+# 14
+
+# Return an array containing the colors of the fruits and the sizes of the
+# vegetables. The sizes should be uppercase and the colors should be capitalized.
+# Output: [["Red", "Green"], "MEDIUM", ["Red", "Green"], ["Orange"], "LARGE"]
+
+hsh = {
+  'grape' => {type: 'fruit', colors: ['red', 'green'], size: 'small'},
+  'carrot' => {type: 'vegetable', colors: ['orange'], size: 'medium'},
+  'apple' => {type: 'fruit', colors: ['red', 'green'], size: 'medium'},
+  'apricot' => {type: 'fruit', colors: ['orange'], size: 'medium'},
+  'marrow' => {type: 'vegetable', colors: ['green'], size: 'large'},
+}
+
+result = hsh.values.map do |item|
+  if item[:type] == 'fruit'
+    item[:colors].map(&:capitalize)
+  else
+    item[:size].upcase
+  end
+end
+
+p result
+
+############################################################
+# 15
+
+# Return an array which contains only the hashes where all the integers are even
+arr = [{a: [1, 2, 3]}, {b: [2, 4, 6], c: [3, 6], d: [4]}, {e: [8], f: [6, 10]}]
+
+
+result = arr.select do |hsh|
+           hsh.all? do |_key, value|
+             value.all? { |num| num.even? }
+           end
+         end
+
+p result

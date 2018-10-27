5e481bb5060fa25eee4ac9952255b08c2fcf3991
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2018-10-27 01:22:18 +0000

    Add squaring_an_argument.rb

---
+++ exercises/easy_3/squaring_an_argument.rb
@@ -0,0 +1,32 @@
+def multiply(num1, num2)
+  num1 * num2
+end
+
+def square(num)
+  multiply(num, num)
+end
+
+def power_of_n(num, exponent)
+  return 1 if exponent == 0
+  return num if exponent == 1
+
+  repetitions = exponent - 1
+  total = num
+  loop do
+    total = multiply(total, num)
+    repetitions -= 1
+    break if repetitions == 0
+  end
+  total
+end
+
+p square(5) == 25
+p square(-8) == 64
+
+p power_of_n(3, 5) == 243
+p power_of_n(5, 5) == 3125
+p power_of_n(2, 6) == 64
+p power_of_n(2, 1) == 2
+p power_of_n(2, 0) == 1
+
+

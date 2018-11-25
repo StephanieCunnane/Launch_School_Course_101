bf62013a029a67bdaf495b6a1ca5c65bc0807688
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2018-11-25 21:01:42 +0000

    Add daily_double.rb

---
+++ exercises/easy_5/daily_double.rb
@@ -0,0 +1,27 @@
+def crunch(str)
+  results = [str[0]]
+  prev_char = str[0]
+  str.each_char do |char|
+    next if char == prev_char
+    results << char
+    prev_char = char
+  end
+  results.join
+end
+
+p crunch('ddaaiillyy ddoouubbllee') == 'daily double'
+p crunch('4444abcabccba') == '4abcabcba'
+p crunch('ggggggggggggggg') == 'g'
+p crunch('a') == 'a'
+p crunch('') == ''
+
+# given solution
+def crunch(text)
+  index = 0
+  crunch_text = ''
+  while index <= text.length - 1
+    crunch_text << text[index] unless text[index] == text[index + 1]
+    index += 1
+  end
+  crunch_text
+end

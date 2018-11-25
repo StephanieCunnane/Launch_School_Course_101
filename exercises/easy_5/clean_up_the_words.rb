3872ad7cd9c31a2b57c12c22a64322d39e9f564b
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2018-11-25 01:24:02 +0000

    Add clean_up_the_words.rb

---
+++ exercises/easy_5/clean_up_the_words.rb
@@ -0,0 +1,36 @@
+def cleanup(str)
+  alphabetic_chars = ('a'..'z').to_a << ' '
+  alphabetic_results = []
+  str.each_char do |char|
+    alphabetic_results << (alphabetic_chars.include?(char) ? char : ' ')
+  end
+
+  no_duplicate_spaces = []
+  no_duplicate_spaces << ' ' if alphabetic_results[0] == ' '
+  previous_char = ' '
+  alphabetic_results.each do |char|
+    next if char == ' ' && previous_char == ' '
+    previous_char = char
+    no_duplicate_spaces << char
+  end
+  no_duplicate_spaces.join
+end
+
+def cleanup(str)
+  str.chars.map { |char| char.match(/[a-z]/i) ? char : ' ' }.join.squeeze(' ')
+end
+
+p cleanup("---what's my +*& line?") == ' what s my line '
+p cleanup("---") == ' '
+p cleanup("---a") == ' a'
+p cleanup("6*($") == ' '
+
+# given solution
+def cleanup(text)
+  text.gsub(/[^a-z]/i, ' ').squeeze(' ')
+end
+
+# or
+def cleanup(text)
+  text.gsub(/[^a-z]/i, ' ').gsub(/\s+/, ' ')
+end

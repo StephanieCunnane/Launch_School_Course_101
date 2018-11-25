ef67b5d280a1240cab72532a203dc64350dd1899
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2018-11-25 18:28:03 +0000

    Add alphabetical_numbers.rb

---
+++ exercises/easy_5/alphabetical_numbers.rb
@@ -0,0 +1,34 @@
+INT_TO_WORD = {
+  0 => 'zero',
+  1 => 'one',
+  2 => 'two',
+  3 => 'three',
+  4 => 'four',
+  5 => 'five',
+  6 => 'six',
+  7 => 'seven',
+  8 => 'eight',
+  9 => 'nine',
+  10 => 'ten',
+  11 => 'eleven',
+  12 => 'twelve',
+  13 => 'thirteen',
+  14 => 'fourteen',
+  15 => 'fifteen',
+  16 => 'sixteen',
+  17 => 'seventeen',
+  18 => 'eighteen',
+  19 => 'nineteen'
+}
+
+def alphabetic_number_sort(int_arr)
+  int_arr.sort_by { |int| INT_TO_WORD[int] }
+end
+
+# or
+
+def alphabetic_number_sort(int_arr)
+  int_arr.sort { |a, b| INT_TO_WORD[a] <=> INT_TO_WORD[b] }
+end
+
+p alphabetic_number_sort((0..19).to_a) == [8, 18, 11, 15, 5, 4, 14, 9, 19, 1, 7, 17, 6, 16, 10, 13, 3, 12, 2, 0]

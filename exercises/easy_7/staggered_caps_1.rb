3f112733c1bc4eed92659f6245eafb43f7626ea9
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2018-12-05 01:45:55 +0000

    staggered_caps_1.rb

---
+++ exercises/easy_7/staggered_caps_1.rb
@@ -0,0 +1,37 @@
+def staggered_case(str)
+  str.chars.map.with_index do |char, idx|
+    idx.even? ? char.upcase : char.downcase
+  end.join
+end
+
+# given solution
+def staggered_case(str)
+  result = ''
+  need_upper = true
+  str.chars.each do |char|
+    if need_upper
+      result += char.upcase
+    else
+      result += char.downcase
+    end
+    need_upper = !need_upper
+  end
+  result
+end
+
+def staggered_case(str, first_char_upper: true)
+  result = ''
+  str.chars.each do |char|
+    if first_char_upper
+      result += char.upcase
+    else
+      result += char.downcase
+    end
+    first_char_upper = !first_char_upper
+  end
+  result
+end
+
+p staggered_case('I Love Launch School!') == 'I LoVe lAuNcH ScHoOl!'
+p staggered_case('ALL_CAPS') == 'AlL_CaPs'
+p staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 NuMbErS'

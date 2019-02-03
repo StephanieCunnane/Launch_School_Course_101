4d4dc864c9098b5444f38517648756b72bf157de
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2019-02-03 15:45:44 -0800

    Add triangle_sides.rb

---
+++ exercises/medium_2/triangle_sides.rb
@@ -0,0 +1,19 @@
+def triangle(side1, side2, side3)
+  triangle_sides = [side1, side2, side3]
+  
+  return :invalid if triangle_sides.any? { |side| side <= 0 }
+  return :invalid if triangle_sides.min(2).sum <= triangle_sides.max
+  
+  case triangle_sides.uniq.size
+  when 1 then :equilateral
+  when 2 then :isosceles
+  when 3 then :scalene
+  end
+end
+
+p triangle(3, 3, 3) == :equilateral
+p triangle(3, 3, 1.5) == :isosceles
+p triangle(3, 4, 5) == :scalene
+p triangle(0, 3, 3) == :invalid
+p triangle(3, 1, 1) == :invalid
+

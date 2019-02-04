cbd2db322b1646b4e4d59902bfbbcea7f83209da
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2019-02-03 19:11:34 -0800

    Add triangles.rb

---
+++ exercises/medium_2/triangles.rb
@@ -0,0 +1,19 @@
+def triangle(angle1, angle2, angle3)
+  angles = [angle1, angle2, angle3]
+  
+  return :invalid unless angles.sum == 180
+  return :invalid if angles.include?(0)
+  
+  case 
+  when angles.include?(90) then :right
+  when angles.max > 90 then :obtuse
+  when angles.min < 90 then :acute
+  end
+end
+
+p triangle(60, 70, 50) == :acute
+p triangle(30, 90, 60) == :right
+p triangle(120, 50, 10) == :obtuse
+p triangle(0, 90, 90) == :invalid
+p triangle(50, 50, 50) == :invalid
+

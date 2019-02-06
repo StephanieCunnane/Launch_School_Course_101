47bd8e004a2b6113372a9d4f4f70612251209d92
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2019-02-05 16:42:50 -0800

    Add transpose_3x3.rb

---
+++ exercises/advanced_1/transpose_3x3.rb
@@ -0,0 +1,22 @@
+def transpose(matrix)
+  result = []
+  (0..2).each do |column_idx|
+    new_row = (0..2).map { |row_idx| matrix[row_idx][column_idx] }
+    result << new_row
+  end
+  result
+end
+
+
+matrix = [
+  [1, 5, 8],
+  [4, 7, 2],
+  [3, 9, 6]
+]
+
+
+new_matrix = transpose(matrix)
+
+p new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
+p matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]]
+

89584d6606091b5de6ea03f919eb85dbcdc3740c
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2019-02-18 14:49:00 -0800

    Add additional sum_of_digits.rb solutions

--- exercises/easy_1/sum_of_digits.rb
+++ exercises/easy_1/sum_of_digits.rb
@@ -5,6 +5,22 @@ def sum(num)
   num.digits.sum
 end
 
+# Or
+def sum(int)
+  int.to_s.chars.map(&:to_i).sum
+end
+
+# Or
+def sum(int)
+  temp_arr = []
+  loop do
+    int, remainder = int.divmod(10)
+    temp_arr << remainder
+    break if int == 0
+  end
+  temp_arr.sum
+end
+
 # test cases
 puts sum(23) == 5
 puts sum(496) == 19

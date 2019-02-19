bca4c646e483591d10f2a5fce6da65e3e6444720
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2019-02-19 09:55:20 -0800

    Add an additional solution for counting_the_number_of_characters.rb

--- exercises/easy_3/counting_the_number_of_characters.rb
+++ exercises/easy_3/counting_the_number_of_characters.rb
@@ -7,3 +7,10 @@ input_string = gets.chomp
 char_count = input_string.delete(' ').length
 
 puts "There are #{char_count} characters in \"#{input_string}\"."
+
+# Or
+print "Please write word or multiple words: "
+phrase = gets.chomp
+char_count = phrase.count('^ ')
+
+puts %(There are #{char_count} characters in "#{phrase}".)

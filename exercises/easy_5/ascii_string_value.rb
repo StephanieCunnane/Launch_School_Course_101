9c627a9054c0fedf039b789c27704d78d289bba8
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2019-02-22 14:10:47 -0800

    Add additional solution for ascii_string_value.rb

--- exercises/easy_5/ascii_string_value.rb
+++ exercises/easy_5/ascii_string_value.rb
@@ -11,6 +11,10 @@ def ascii_value(str)
   sum
 end
 
+def ascii_value(str)
+  str.chars.reduce(0) { |ascii_sum, char| ascii_sum += char.ord }
+end
+
 p ascii_value('Four score') == 984
 p ascii_value('Launch School') == 1251
 p ascii_value('a') == 97

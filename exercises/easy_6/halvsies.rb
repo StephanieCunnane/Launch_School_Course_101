b0b900ec990ddf05664a6b33a80d76c7985a43f1
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2019-03-05 09:26:33 -0800

    Add additional solution to halvsies.rb

--- exercises/easy_6/halvsies.rb
+++ exercises/easy_6/halvsies.rb
@@ -21,6 +21,15 @@ def halvsies(array)
   [first_half, second_half]
 end
 
+def halvsies(arr)
+  arr = arr.dup
+  results = []
+  midpoint = arr.size / 2
+
+  results << (arr.size.even? ? arr.slice!(0, midpoint) : arr.slice!(0..midpoint) )
+  results << arr
+end
+
 p halvsies([1, 2, 3, 4]) == [[1, 2], [3, 4]]
 p halvsies([1, 5, 2, 4, 3]) == [[1, 5, 2], [4, 3]]
 p halvsies([5]) == [[5], []]

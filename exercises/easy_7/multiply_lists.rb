42af2a8567e7a5bf5b98b38ae0a6e4f1cb39a203
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2019-03-06 08:29:06 -0800

    Add additional solution for multiply_lists.rb

--- exercises/easy_7/multiply_lists.rb
+++ exercises/easy_7/multiply_lists.rb
@@ -2,6 +2,10 @@ def multiply_list(arr1, arr2)
   arr1.zip(arr2).map { |pair| pair.first * pair.last }
 end
 
+def multiply_list(arr1, arr2)
+  arr1.zip(arr2).map { |sub_arr| sub_arr.reduce(:*) }
+end
+
 # given solution
 def multiply_list(arr1, arr2)
   products = []

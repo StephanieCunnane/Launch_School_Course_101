32800ac1aeb1a934a75281c36ebcea2070a97413
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2019-03-07 08:40:53 -0800

    Add additional solution for double_char_2.rb

--- exercises/easy_8/double_char_2.rb
+++ exercises/easy_8/double_char_2.rb
@@ -9,6 +9,15 @@ def double_consonants(str)
   results
 end
 
+# or
+CONSONANTS = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+
+def double_consonants(str)
+  str.chars.map do |char|
+    CONSONANTS.include?(char.downcase) ? char + char : char
+  end.join
+end
+
 puts double_consonants('String') == "SSttrrinngg"
 puts double_consonants("Hello-World!") == "HHellllo-WWorrlldd!"
 puts double_consonants("July 4th") == "JJullyy 4tthh"

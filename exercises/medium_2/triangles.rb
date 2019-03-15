9ac75d9b492a3ab0b88acfa843bc5de8edb59260
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2019-03-15 08:17:00 -0700

    Refactor triangles.rb

--- exercises/medium_2/triangles.rb
+++ exercises/medium_2/triangles.rb
@@ -7,7 +7,20 @@ def triangle(angle1, angle2, angle3)
   case 
   when angles.include?(90) then :right
   when angles.max > 90 then :obtuse
-  when angles.min < 90 then :acute
+  when angles.max < 90 then :acute
+  end
+end
+
+def triangle(angle1, angle2, angle3)
+  angles = [angle1, angle2, angle3]
+
+  return :invalid if angles.include?(0)
+  return :invalid unless angles.sum == 180
+
+  case
+  when angles.one?(90) then :right
+  when angles.all? { |angle| angle < 90 } then :acute
+  else :obtuse
   end
 end
 

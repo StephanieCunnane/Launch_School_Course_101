da4b9a48388f3ff4cca99f9277454c75031c1701
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2019-03-15 07:52:58 -0700

    Add additional solution to triangle_sides.rb

--- exercises/medium_2/triangle_sides.rb
+++ exercises/medium_2/triangle_sides.rb
@@ -11,6 +11,21 @@ def triangle(side1, side2, side3)
   end
 end
 
+# Or
+def triangle(side1, side2, side3)
+  sides = [side1, side2, side3]
+
+  return :invalid if sides.include?(0)
+  return :invalid if sides.min(2).sum <= sides.max
+
+  unique_lengths = sides.uniq.size
+  case unique_lengths
+  when 1 then :equilateral
+  when 2 then :isosceles
+  else :scalene
+  end
+end
+
 p triangle(3, 3, 3) == :equilateral
 p triangle(3, 3, 1.5) == :isosceles
 p triangle(3, 4, 5) == :scalene

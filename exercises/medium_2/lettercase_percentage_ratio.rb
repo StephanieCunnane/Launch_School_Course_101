dbdb4c6c9672aefda5411ebbf76d83aa7c14c7bf
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2019-03-15 07:47:06 -0700

    Add additional solution for lettercase_percentage_ratio.rb

--- exercises/medium_2/lettercase_percentage_ratio.rb
+++ exercises/medium_2/lettercase_percentage_ratio.rb
@@ -19,6 +19,37 @@ def letter_percentages(str)
   percentages 
 end
 
+# Additional solution
+def letter_percentages(str)
+  total_chars = str.size
+  lowercase = str.scan(/[a-z]/).count
+  uppercase = str.scan(/[A-Z]/).count
+  neither = total_chars - (lowercase + uppercase)
+  
+  percentages = {}
+  percentages[:lowercase] = lowercase.fdiv(total_chars) * 100
+  percentages[:uppercase] = uppercase.fdiv(total_chars) * 100
+  percentages[:neither] = neither.fdiv(total_chars) * 100
+  percentages
+end
+
+# Refactoring
+def calculate_percentages(count, total)
+  (count.fdiv(total) * 100).round(2)
+end
+
+def letter_percentages(str)
+  total_chars = str.size
+  lowercase = str.scan(/[a-z]/).count
+  uppercase = str.scan(/[A-Z]/).count
+  neither = total_chars - (lowercase + uppercase)
+  
+  percentages = {}
+  percentages[:lowercase] = calculate_percentages(lowercase, total_chars)
+  percentages[:uppercase] = calculate_percentages(uppercase, total_chars)
+  percentages[:neither] = calculate_percentages(neither, total_chars)
+  percentages
+end
 
 # Given solution
 def calculate(percentages, counts, length)
